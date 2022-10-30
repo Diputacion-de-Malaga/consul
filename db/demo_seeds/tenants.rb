d0bf3f82454d3d630c3af77565e12972973af293
Javi Martín <javim@elretirao.net>
2022-10-30 12:37:33 +0100

    EXPERIMENT: different locales per tenant TODO: doesn't work right now

--- db/demo_seeds/tenants.rb
+++ db/demo_seeds/tenants.rb
@@ -2,4 +2,16 @@ section "Creating DEMO Tenants" do
   Tenant.create!(name: "Mega CONSUL", schema: "megacity")
   Tenant.create!(name: "Micro CONSUL", schema: "microvillage")
   Tenant.create!(name: "Middle CONSUL", schema: "middletown")
+
+  Tenant.switch("megacity") do
+    Setting["locales.available"] = "en,es,sv,tr,zh-TW"
+  end
+
+  Tenant.switch("microvillage") do
+    Setting["locales.available"] = "en,ar,bg,cs"
+  end
+
+  Tenant.switch("middletown") do
+    Setting["locales.available"] = "en,de,he,ro"
+  end
 end
