4339faae28765534055cc1e894656251653510ef
taitus <sebastia.roig@gmail.com>
2023-05-24 17:19:10 +0200

    Allow undo votes for debates

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -108,8 +108,27 @@ class Debate < ApplicationRecord
   def register_vote(user, vote_value)
     if votable_by?(user)
       Debate.increment_counter(:cached_anonymous_votes_total, id) if user.unverified? && !user.voted_for?(self)
-      vote_by(voter: user, vote: vote_value)
+      if undo_vote?(user, vote_value)
+        undo_vote(user, vote_value)
+      else
+        vote_by(voter: user, vote: vote_value)
+      end
     end
+  end
+
+  def undo_vote?(user, vote_value)
+    old_vote_value = user.voted_as_when_voted_for(self)
+    new_vote_value = parse_vote_value(vote_value)
+
+    old_vote_value == new_vote_value
+  end
+
+  def undo_vote(user, vote_value)
+    parse_vote_value(vote_value) ? self.unliked_by(user) : self.undisliked_by(user)
+  end
+
+  def parse_vote_value(vote_value)
+    vote_value == "yes" ? true : false
   end
 
   def votable_by?(user)
