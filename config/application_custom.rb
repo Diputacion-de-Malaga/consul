f77ebaa23393948769b833e6d5c3ac5009868e4c
Diego Calvo Castillo <diegocc6@gmail.com>
2023-10-11 14:14:47 +0200

    events page in header es/en

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,20 @@
 module Consul
   class Application < Rails::Application
+    config.i18n.default_locale = :es
+    available_locales = [
+      "es",
+      "en"
+    ]
+    config.i18n.available_locales = available_locales
+    config.i18n.fallbacks = {
+      "en"    => "es"
+    }
+
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**[^custom]*", "*.{rb,yml}")]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "custom", "**", "*.{rb,yml}")]
+
+    config.after_initialize do
+      Globalize.set_fallbacks_to_all_available_locales
+    end
   end
 end
